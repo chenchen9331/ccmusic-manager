0cf11a42b124454157ace34bba02abcb969a2d66
chenchen9331 <65482722+chenchen9331@users.noreply.github.com>
2021-02-07 11:00:51 +0800

    Update http.js

--- src/api/http.js
+++ src/api/http.js
@@ -7,7 +7,7 @@ axios.defaults.withCredentials = true;
 // Content-Type响应头
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=UTF-8';
 // base url
-axios.defaults.baseURL = "http://123.56.120.185:8888";
+axios.defaults.baseURL = "http://127.0.0.1:8888";
 
 // 响应拦截器
 axios.interceptors.response.use(
